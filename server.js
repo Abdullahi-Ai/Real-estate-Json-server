b011eab6fac90ecb1083b39c343a7e2a4819ce2e
Abdullahi-Ai <abdullahiibrashair@gmail.com>
2025-04-25 21:05:23 +0300

    created json server

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use('/api', router)
-
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://phase-2-group-project-pink.vercel.app/') // The URL you put here is for the web application that you have deployed using Github Pages
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+})
 server.listen(process.env.PORT || 5000, () => {
     console.log('JSON Server is running')
 })
